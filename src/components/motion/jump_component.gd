1998f26e9c8df4aa38be4895072facf6fff040bd
gabriel <gabrieldeolival2@gmail.com>
2026-03-30 16:34:51 -0400

    fix(jump): fix double jump reset and coyote time behavior

--- src/components/motion/jump_component.gd
+++ src/components/motion/jump_component.gd
@@ -1,16 +1,22 @@
 extends Node2D
 class_name JumpComponent
 
-var max_jumps: int = 2
+@export var total_allowed_jumps: int = 2
+var jumps_available: int = 0
 
 func apply_jump(player: CharacterBody2D, jump_velocity: float, coyote_timer: float, input_buffer: InputComponent) -> void:
-	if input_buffer.consume_action("jump") and (player.is_on_floor() or player.coyote_time < coyote_timer or max_jumps > 0):
-		execute_jump(player, jump_velocity, coyote_timer)
-		max_jumps -= 1
-	elif player.is_on_floor():
-		max_jumps = 2
+	if player.is_on_floor():
+		jumps_available = total_allowed_jumps
+	
+	if not player.is_on_floor() and player.coyote_time > coyote_timer and jumps_available == total_allowed_jumps:
+		jumps_available = total_allowed_jumps - 1
+	
+	if input_buffer.consume_action("jump"):
+		if jumps_available > 0:
+			execute_jump(player, jump_velocity)
+			jumps_available -= 1
 
-func execute_jump(player: CharacterBody2D, jump_velocity: float, coyote_timer: float) -> void:
+func execute_jump(player: CharacterBody2D, jump_velocity: float) -> void:
 	player.velocity.y = 0
 	player.velocity.y = jump_velocity
-	player.coyote_time = coyote_timer
+	player.coyote_time = 999
